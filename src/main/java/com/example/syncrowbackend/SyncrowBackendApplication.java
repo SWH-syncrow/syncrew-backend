e3596119e630a07f348e6b9d409cc3816f44d385
SoMi Park <fsm1204@naver.com>
2023-07-18 22:25:10 +0900

    first commit

---
+++ src/main/java/com/example/syncrowbackend/SyncrowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.syncrowbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncrowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncrowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/syncrowbackend/SyncrowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.syncrowbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncrowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
