ac371c01308af853640410af81e92a1acf69facf
SoMi Park <fsm1204@naver.com>
2023-07-22 19:02:47 +0900

    feat: create Friendrequest and User entities

---
+++ src/main/java/com/example/syncrowbackend/friendrequest/Friendrequest.java
@@ -0,0 +1,20 @@
+package com.example.syncrowbackend.friendrequest;
+
+import com.example.syncrowbackend.base.entity.BaseTimeEntity;
+import com.example.syncrowbackend.user.entity.User;
+import jakarta.persistence.*;
+
+@Entity
+public class Friendrequest extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "from_id", referencedColumnName = "id")
+    private User fromUser;
+
+    @ManyToOne
+    @JoinColumn(name = "to_id", referencedColumnName = "id")
+    private User toUser;
+}
