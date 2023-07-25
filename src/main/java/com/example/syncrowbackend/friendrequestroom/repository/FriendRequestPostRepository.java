59972ae57725c688d126a4988da13fae006be24a
SoMi Park <fsm1204@naver.com>
2023-07-25 13:54:30 +0900

    refactor: add @Repository annotation

--- src/main/java/com/example/syncrowbackend/friendrequestroom/repository/FriendRequestPostRepository.java
+++ src/main/java/com/example/syncrowbackend/friendrequestroom/repository/FriendRequestPostRepository.java
@@ -4,7 +4,9 @@ import com.example.syncrowbackend.friendrequestroom.entity.FriendRequestPost;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FriendRequestPostRepository extends JpaRepository<FriendRequestPost, Long> {
     Page<FriendRequestPost> findAll(Pageable pageable);
     Page<FriendRequestPost> findByUserId(Long id, Pageable pageable);
