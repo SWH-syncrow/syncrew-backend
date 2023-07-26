aad91c4854668eff3c1b8a94b894f3da54c81809
Eunbi Kim <eunbkim4@gmail.com>
2023-07-26 15:40:19 +0900

    chore: 필요 없는 파일 삭제

--- src/main/java/com/example/syncrowbackend/global/error/AuthenticationException.java
+++
@@ -1,7 +0,0 @@
-package com.example.syncrowbackend.global.error;
-
-public class AuthenticationException extends BusinessException {
-    public AuthenticationException(ErrorCode errorCode) {
-        super(errorCode);
-    }
-}
