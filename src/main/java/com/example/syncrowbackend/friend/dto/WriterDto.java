149ce431f1cecfc5e6ca7703e73545cd7c769963
SoMi Park <fsm1204@naver.com>
2023-08-01 20:29:03 +0900

    feat: Change return type of Page<PostDto> to Page<GetUserResponseDto>

---
+++ src/main/java/com/example/syncrowbackend/friend/dto/WriterDto.java
@@ -0,0 +1,30 @@
+package com.example.syncrowbackend.friend.dto;
+
+import com.example.syncrowbackend.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class WriterDto{
+    private Long id;
+    private String username;
+    private String profileImage;
+    private Double temp;
+
+    @Builder
+    public static WriterDto toDto(User user) {
+        if(user == null)
+            return null;
+
+        return WriterDto.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .profileImage(user.getProfileImage())
+                .temp(user.getTemp()).build();
+    }
+}
